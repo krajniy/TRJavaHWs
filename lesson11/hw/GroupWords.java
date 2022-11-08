f54e2c45ed27a3d18de55facee304ae2f7967aac
krajniy <krajniy@gmail.com>
2022-11-08 20:50:14 +0100

    HW 11 improvements

--- lesson11/hw/GroupWords.java
+++ lesson11/hw/GroupWords.java
@@ -1,5 +1,7 @@
 package TRJavaHWs.lesson11.hw;
 
+import java.util.*;
+
 public class GroupWords {
     /*
     First level: 1. Сгруппируйте слова с одинаковым набором символов
@@ -16,22 +18,80 @@ public class GroupWords {
     public static void main(String[] args) {
         String words[] = {"student", "students", "dog", "god", "cat", "act", "flow", "wolf"};
         findSameUniqChar(words);
+        groupWords(words);
 
     }
 
-    public static void findSameUniqChar(String[] strings){
+    private static void groupWords(String[] strings) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strings){
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+
+//           Object[] chch = Stream.of(s.toCharArray()).distinct().toArray();
+
+            Map<Character, String> sMap = new HashMap<>();
+            for (int i = 0; i < chars.length; i++) {
+                sMap.put(chars[i], s);
+            }
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<Character, String> pair : sMap.entrySet()){
+                sb.append(pair.getKey());
+            }
+
+            String str = String.valueOf(sb);
+            List<String> list = new ArrayList<>();
+            list.add(s);
+            List<String> values = map.getOrDefault(str, list);
+
+            if (!values.contains(s)){
+                values.add(s);
+            }
+            map.putIfAbsent(str, values);
+
+        }
+        for (Map.Entry<String, List<String>> e : map.entrySet()){
+            System.out.println(e.getValue());
+
+        }
+
+    }
+
+    public static void findSameUniqChar(String[] strings) {
         for (int i = 0; i < strings.length; i++) {
             for (int j = 1; j < strings.length; j++) {
-                if (primitiveHash(strings[i]) == primitiveHash(strings[j])){
-                    System.out.println(strings[i] + ", " + strings[j]);
-                    i++;
-                    break;
+                if (strings[i].length() == strings[j].length()) {
+                    if (primitiveHash(strings[i]) == primitiveHash(strings[j])) {
+                        System.out.println(strings[j] + ", " + strings[i]);
+                        i++;
+                        break;
+                    }
+                }
+                else {
+                   //TODO
+                    Set<Character> set1 = new TreeSet<>();
+                    Set<Character> set2 = new TreeSet<>();
+                    for (int k = 0; k < strings[i].length(); k++) {
+                        set1.add(strings[i].charAt(k));
+                    }
+                    for (int k = 0; k < strings[j].length(); k++) {
+                        set2.add(strings[j].charAt(k));
+                    }
+                    if (primitiveHash(strings[i]) == primitiveHash(strings[j])) {
+                        System.out.println(strings[i] + ", " + strings[j]);
+                        i++;
+                        break;
+                    }
+
+
+
+
                 }
             }
         }
     }
 
-    public static int primitiveHash(String string){
+    public static int primitiveHash(String string) {
         int hash = 0;
         for (int i = 0; i < string.length(); i++) {
             char c = string.charAt(i);
