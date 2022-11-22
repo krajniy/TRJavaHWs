0ec8fe0698ae1f958ffba7acd76b83adb56bdb36
krajniy <krajniy@gmail.com>
2022-11-22 21:34:23 +0100

    lesson 16 practice pt3

--- lesson16/StreamPractice.java
+++ lesson16/StreamPractice.java
@@ -2,6 +2,7 @@ package TRJavaHWs.lesson16;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamPractice {
     public static void main(String[] args) {
@@ -12,6 +13,9 @@ public class StreamPractice {
         System.out.println(countOccurrences(List.of("a", "b", "a", "c", "a", "d", "s", "x", "a"), "a"));
         System.out.println(findMin(list));
         System.out.println(findMax(list));
+
+        System.out.println(sortReverseUnique(List.of("a1", "b5", "c1", "a2", "b4", "c1", "a1")));
+
     }
 
     public static int oddSum(List<Integer> list){
@@ -32,24 +36,32 @@ public class StreamPractice {
     }
 
 
+    //TODO: написать код через стрим, который будет проверять сколько раз word встречается в list
     public static int countOccurrences(List<String> list, String word){
         return (int) list.stream()
                 .filter(word::equals)
                 .count();
     }
 
+    //TODO: написать код, который с помощью стримов будет искать наименьший элемент
     public static int findMin(List<Integer> list){
         return list.stream()
                 .min(Integer::compareTo)
                 .get();
-
     }
 
+    //TODO: написать код, который с помощью стримов будет искать наибольший элемент
     public static int findMax(List<Integer> list){
         return list.stream()
                 .max(Integer::compareTo)
                 .get();
 
+    }
+
+
+    //TODO: написать стрим, который отсортирует коллекцию в обратном порядке и удалит дубликаты
+    public static List<String> sortReverseUnique(List<String> list){
+        return list.stream().distinct().sorted(String::compareTo).collect(Collectors.toList());
     }
 
 
