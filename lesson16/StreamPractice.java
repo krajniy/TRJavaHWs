5a768f620a017cb1eca8ffd3de08dfd0be953f21
krajniy <krajniy@gmail.com>
2022-11-22 21:14:09 +0100

    lesson 16 practice pt2

--- lesson16/StreamPractice.java
+++ lesson16/StreamPractice.java
@@ -9,8 +9,9 @@ public class StreamPractice {
         System.out.println(oddSum(list));
         System.out.println(oddSumStream(list));
 
-
-
+        System.out.println(countOccurrences(List.of("a", "b", "a", "c", "a", "d", "s", "x", "a"), "a"));
+        System.out.println(findMin(list));
+        System.out.println(findMax(list));
     }
 
     public static int oddSum(List<Integer> list){
@@ -28,4 +29,21 @@ public class StreamPractice {
                 .filter(e -> e % 2 == 1)
                 .reduce(Integer::sum).get();
     }
+
+
+    public static int countOccurrences(List<String> list, String word){
+        return (int) list.stream().filter(word::equals).count();
+    }
+
+    public static int findMin(List<Integer> list){
+        return list.stream().min(Integer::compareTo).get();
+
+    }
+
+    public static int findMax(List<Integer> list){
+        return list.stream().max(Integer::compareTo).get();
+
+    }
+
+
 }
