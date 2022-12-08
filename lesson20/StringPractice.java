5ecae7d64794415e16318b9d9a0562715488bfb6
krajniy <krajniy@gmail.com>
2022-12-08 21:38:36 +0100

    practice lesson 20

--- lesson20/StringPractice.java
+++ lesson20/StringPractice.java
@@ -11,7 +11,7 @@ public class StringPractice {
         String check = scanner.nextLine();
         scanner.close();
 
-        System.out.println(countEachLetter(check.toLowerCase()));
+        System.out.println(countEachLetter(check));
 
     }
     static Map<Character, Integer> countEachLetter(String s){
