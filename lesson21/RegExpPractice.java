03beda2add2ccd388290ac46e9d4525c5ecfc1a7
krajniy <krajniy@gmail.com>
2022-12-12 21:21:29 +0100

    lesson 21 practice

--- lesson21/RegExpPractice.java
+++ lesson21/RegExpPractice.java
@@ -7,11 +7,14 @@ public class RegExpPractice {
     public static void main(String[] args) {
         String s = "www.telran.de";
         String s1 = "https://www.google.com";
+        String s2 = "http://www.google.com";
+
 
         Pattern p = Pattern.compile("(https?://)?(www\\.)?.+\\.(com|de)");
 
         System.out.println(p.matcher(s).matches());         // true
         System.out.println(p.matcher(s1).matches());        // true
+        System.out.println(p.matcher(s2).matches());        // true
 
     }
 }
