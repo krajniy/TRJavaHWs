0ab8f81ede9db93c1417ac0cb8b671b211a49aec
krajniy <krajniy@gmail.com>
2022-12-15 21:10:32 +0100

    lesson 23 practice

--- lesson23/testing/Main.java
+++ lesson23/testing/Main.java
@@ -6,11 +6,20 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        User u = createUser(
+        User admin = createUser(
                 "John Smith", LocalDate.of(1989, 12, 13),
                 UserType.ADMIN, "@jsmith89");
 
-        System.out.println(u);
+        User defaultUser = createUser(
+                "Tyler Black", LocalDate.of(1963, 9, 24),
+                UserType.DEFAULT, "@jsmith89");
+
+        System.out.println(createUserTest(admin));
+        System.out.println(createUserTest(defaultUser));
+
+        System.out.println(createUserNegativeTest(admin, true));
+        System.out.println(createUserNegativeTest(defaultUser, false));
+
 
     }
 
@@ -18,17 +27,43 @@ public class Main {
         String name = fullName.split(" ")[0];
         String lastName = fullName.split(" ")[1];
         int age = LocalDate.now().compareTo(birthDate);
-        boolean isAdmin = userType==UserType.ADMIN;
+        boolean isAdmin = true; //userType == UserType.ADMIN;
         login = login.replaceAll("@", "");
 
         return new User(name, lastName, age, isAdmin, login);
 
 
-
-        //String name, String lastName, int age, boolean isAdmin, String login
     }
 
-    private static enum UserType {
+    public static boolean createUserTest(User u) {
+        boolean age = u.getAge()>0;
+        boolean name = !u.getName().isEmpty();
+        boolean lastName = !u.getLastName().isEmpty();
+        boolean admin = true;
+        boolean login = u.getLogin().matches("\\w+");
+
+
+        if (age && name && lastName && admin && login){
+            return true;
+        } else throw new  RuntimeException();
+
+    }
+
+    public static boolean createUserNegativeTest(User u, boolean isAdmin) {
+        boolean age = u.getAge()>0;
+        boolean name = !u.getName().isEmpty();
+        boolean lastName = !u.getLastName().isEmpty();
+        boolean admin = isAdmin == u.isAdmin();
+        boolean login = u.getLogin().matches("\\w+");
+
+
+        if (age && name && lastName && admin && login){
+            return true;
+        } else throw new  RuntimeException();
+
+    }
+
+    private enum UserType {
         ADMIN, DEFAULT
     }
 
