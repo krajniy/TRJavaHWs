018c25b7bcf35fd3b2531b04bcfd41ae46f98b55
krajniy <krajniy@gmail.com>
2023-01-12 20:41:06 +0100

    classwork practice 27

---
+++ lesson27/Sync.java
@@ -0,0 +1,39 @@
+package TRJavaHWs.lesson27;
+
+public class Sync {
+    public static void main(String[] args) throws InterruptedException {
+    Incrementer inc = new Incrementer();
+//        inc.increment();
+//        inc.increment();
+//        inc.increment();
+//        inc.increment();
+//        inc.increment();
+//        inc.increment();
+//        inc.increment();
+
+        for (int i = 0; i < 4; i++) {
+            (new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    inc.increment();
+                }
+
+            })).start();
+        }
+
+        Thread.sleep(1000);
+        System.out.println(inc.getCount());
+    }
+
+
+}
+class Incrementer {
+    private int count;
+     public synchronized void  increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
