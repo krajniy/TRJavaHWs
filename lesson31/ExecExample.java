9112470bad82ec968c3209d98bbb38f94121050b
krajniy <krajniy@gmail.com>
2023-01-26 21:54:41 +0100

    practice 31

---
+++ lesson31/ExecExample.java
@@ -0,0 +1,126 @@
+package TRJavaHWs.lesson31;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ExecExample implements ExecutorService {
+
+    private Thread thread1 = new Thread();
+    private Thread thread2 = new Thread();
+
+
+    //TODO
+    @Override
+    public void shutdown() {
+        thread1.interrupt();
+        thread2.interrupt();
+
+    }
+
+    //TODO
+    @Override
+    public Future<?> submit(Runnable task) {
+        Thread thread = new Thread(task);
+
+        if (!thread1.isAlive()){
+            thread1 = thread;
+            thread1.start();
+        } else if (!thread1.isAlive()){
+            thread2 = thread;
+            thread2.start();
+        }
+
+
+
+
+        return new Future<Object>() {
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                return false;
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return false;
+            }
+
+            @Override
+            public boolean isDone() {
+                return false;
+            }
+
+            @Override
+            public Object get() throws InterruptedException, ExecutionException {
+                return null;
+            }
+
+            @Override
+            public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+                return null;
+            }
+        };
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        return null;
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return false;
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return null;
+    }
+
+    @Override
+    public <T> Future<T> submit(Runnable task, T result) {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public void execute(Runnable command) {
+
+    }
+}
